ab3625df16c5b5101ad4d479e342e8a17676a346
AgentBraun <AgentBraun@users.noreply.github.com>
2023-05-22 16:06:32 +0200

    Added express router and controllers. working get and post notes api endpoints

--- backend/src/models/note.ts
+++ backend/src/models/note.ts
@@ -1,5 +1,13 @@
-import { Schema } from "mongoose";
+import { InferSchemaType, Schema, model } from 'mongoose';
 
-const noteSchema = new Schema({
-  title: { type: }
-})
+const noteSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    text: { type: String },
+  },
+  { timestamps: true }
+);
+
+type Note = InferSchemaType<typeof noteSchema>;
+
+export default model<Note>('Note', noteSchema);
